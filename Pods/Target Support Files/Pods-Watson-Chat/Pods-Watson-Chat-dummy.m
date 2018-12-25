10b5b2670dcc968e8566aa9b00ef1271f3096d0e
cmcgheit <cmcgheit@Cs-iMac.local>
2018-12-25 10:15:56 -0600

    complete build gutted project

--- Pods/Target Support Files/Pods-Watson-Chat/Pods-Watson-Chat-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Watson_Chat : NSObject
-@end
-@implementation PodsDummy_Pods_Watson_Chat
-@end
--- Pods/Target Support Files/Pods-Watson-ChatTests/Pods-Watson-ChatTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Watson_ChatTests : NSObject
-@end
-@implementation PodsDummy_Pods_Watson_ChatTests
-@end
